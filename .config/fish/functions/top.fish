711cf5fc44b6e33c47af6df6d95de5c855235036
Jack Rickard <jack.rickard@outlook.com>
2021-04-27 00:19:14 +0100

    Replace ytop with bottom

--- .config/fish/functions/top.fish
+++ .config/fish/functions/top.fish
@@ -1,3 +1,4 @@
-function top --wraps=ytop --description 'alias top ytop'
-  ytop -p $argv;
+# Defined in /tmp/fish.seeCuN/top.fish @ line 1
+function top --wraps=ytop --wraps=btm --description 'alias top btm'
+  btm $argv;
 end
