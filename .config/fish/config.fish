54d8cccc88e1512d160eeeab106f37a7a54f53ad
Jack Rickard <jack.rickard@outlook.com>
2024-03-02 21:42:06 +0000

    Various fish things

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
 if status --is-interactive; and test -z "$DISPLAY"; and test "$XDG_VTNR" = 1
-  exec startx -- -keeptty 
+  exec Hyprland 
 end
 
 starship init fish | source
 zoxide init fish | source
-
+direnv hook fish | source
 
 thefuck --alias | source
-alias exit="exit 0"
--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --description 'List contents of directory using long format'
-	exa -alFbghm --time-style=long-iso --git $argv
+	eza -alF -bghm --time-style=long-iso --git $argv
 end
